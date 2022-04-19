05534ef7776daa805785e336cc52cb5817d969d5
ryanallenk <ryanallenk@gmail.com>
2022-04-19 19:48:20 -0400

    add conditional formatting to DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,10 +5,21 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 let listClass = classNames("day-list__item", {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0})
+
+const formatSpots = (spots) => {
+  if (spots === 1) {
+    return `${spots} spot remaining`
+  }
+  if (spots === 0) {
+    return "no spots remaining"
+  }
+  return `${spots} spots remaining`
+}
+
   return (
     <li onClick={() => props.setDay(props.name)} className={listClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
