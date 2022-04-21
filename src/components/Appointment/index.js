87a08e496cda777ad1ef68fae0002debaa1c235f
ryanallenk <ryanallenk@gmail.com>
2022-04-20 22:42:34 -0400

    add schedule to application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,8 @@ import Empty from './Empty'
 
 import "components/Appointment/styles.scss"
 
+
+
 export default function Appointment (props) {
 const interviewCheck = () => {
   return (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>
