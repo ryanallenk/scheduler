9bc963b77b26a1d64be7f5cda3bf7d95fbd433d8
ryanallenk <ryanallenk@gmail.com>
2022-04-20 22:29:38 -0400

    add appointment component functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import Header from './Header'
+import Show from './Show'
+import Empty from './Empty'
+
 import "components/Appointment/styles.scss"
+
 export default function Appointment (props) {
-const body = () => {
-  if (props.time) {
-    return `Appointment at ${props.time}`
-  }
-  return `No Appointments`
-} 
+const interviewCheck = () => {
+  return (props.interview) ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty/>
+}
   return (
     <article className="appointment">
-      {body()}
+      <Header time={props.time}></Header>
+      {interviewCheck()}
+      
     </article>
   )
 }
