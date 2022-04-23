aaa54edc3212e796d144b8006e33a10cb485dc33
ryanallenk <ryanallenk@gmail.com>
2022-04-22 20:44:36 -0400

    add getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,36 +6,43 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
-  const dailyAppointments = getAppointmentsForDay({appointments: state.appointments, days: state.days}, state.day);
-
+  
   const setDay = day => setState({ ...state, day });
-
-
+  
+  
   useEffect (() => {
     Promise.all([
       axios.get('/api/days'),
-      axios.get('/api/appointments')
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ])
     .then (all => {
-      console.log(all)
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
     })
   }, []);
-
+  
+  
+  
+  const dailyAppointments = getAppointmentsForDay({appointments: state.appointments, days: state.days}, state.day);
+  console.log(dailyAppointments)
   const appointmentItems = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview)
+    
     return (
       <Appointment
         key={appointment.id}
         {...appointment}
+        interview={interview}
       />
     )
   })
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,3 +9,13 @@ export function getAppointmentsForDay(state, day) {
   }
   return appointmentsArray
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null
+  }
+  const interviewersObj = state.interviewers;
+  const interviewerKey = interview.interviewer 
+
+  return {...interview, interviewer:interviewersObj[interviewerKey]}
+}
