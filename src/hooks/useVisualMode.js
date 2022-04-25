3a4edfe055c0f7c9c0c9319bf0eda8d0e8eebcc0
ryanallenk <ryanallenk@gmail.com>
2022-04-25 18:33:22 -0400

    fixed useVisualModer helper back method works properly

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,14 +4,24 @@ export default function useVisualMode(initial) {
 const [mode, setMode] = useState(initial);
 const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
+  function transition(newMode, replace = false) {
+    const historyUpdate = [...history];
+    replace && historyUpdate.pop()
+    
+    historyUpdate.push(newMode)
+    
     setMode(newMode);
-    history.push(newMode)
+    setHistory(historyUpdate);
   }
 
   function back () {
-    const backValue = history.splice(-2,1)
-    setMode(backValue[0])
+    const historyUpdate = [...history]
+    if(historyUpdate.length > 1) {
+      historyUpdate.pop()
+    }
+
+    setHistory(historyUpdate)
+    setMode(historyUpdate[historyUpdate.length - 1])
   }
   return {transition, mode, back};
 }
