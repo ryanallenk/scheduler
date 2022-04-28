42fa84473888080403eb026e0a352ce6d9662614
ryanallenk <ryanallenk@gmail.com>
2022-04-28 17:45:10 -0400

    add passing cypress tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,7 @@ const formatSpots = (spots) => {
 }
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={listClass} selected={props.selected}>
+    <li onClick={() => props.setDay(props.name)} className={listClass} selected={props.selected} data-testid="day">
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
